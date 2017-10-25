4ae95be8750a0a09876568dec53359fde4f848e9
Ratna <rmanyala@live.com>
2017-10-25 21:42:24 +0530

    user story #24057 add import

--- src/test/java/rdemo/HelloControllerTest.java
+++ src/test/java/rdemo/HelloControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import com.hpe.alm.octane.OctaneCucumber;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
